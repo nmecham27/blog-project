f8ba8c8c2561042c090f8595668f845f5ac99ae3
Noah Mecham <nmecham27@gmail.com>
2017-04-24 19:16:56 +0000

    added stuff to database in the models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
+class Post(models.Model):
+	text = models.TextField()
+	timeposted = models.DateTimeField(default=timezone.now)
+	title = models.CharField(max_length=150)
+
+	def __str__(self):
+	    return self.title
+
