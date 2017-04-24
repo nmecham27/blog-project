5ed6a08bd1989e1edf5526d5e5918a23be843f9c
Noah Mecham <nmecham27@gmail.com>
2017-04-24 20:21:32 +0000

    added some work on the views and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
