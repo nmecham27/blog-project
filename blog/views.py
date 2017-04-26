84dd2e88b07672ed434db2ffaca1edfae2f43582
Noah Mecham <nmecham27@gmail.com>
2017-04-26 00:36:27 +0000

    you can now add a new post

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'blog/index.html'
@@ -14,6 +16,13 @@ class IndexView(generic.ListView):
         return Post.objects.order_by('-timeposted')[:5]
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.timeposted = timezone.now()
+            post.save()
+            return redirect('index')
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-
