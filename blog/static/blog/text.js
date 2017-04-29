1fd9713a0526d035fa8664c285b05649b5524d34
Noah Mecham <nmecham27@gmail.com>
2017-04-29 18:31:26 +0000

    fixed up the css

--- blog/static/blog/text.js
+++ blog/static/blog/text.js
@@ -1,10 +1,18 @@
-document.getElementById("id_text").addEventListener("keypress",charactercnt);
+document.getElementById("id_text").addEventListener("keydown",charactercnt);
 
-var a = 1;
+var a = 0;
 
 function charactercnt() {
-	var current = document.getElementById("counter");
-	current.value = a;
-	a++;
+	var x = event.keyCode;
+	if(x == 8 && a > 0){
+	    --a;
+	    var current = document.getElementById("counter");
+	    current.value = a;
+	}
+	else if(x != 8){
+	    var current = document.getElementById("counter");
+	    ++a;
+	    current.value = a;
+	}
 }
 
